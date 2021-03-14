78bfbabd628dff4d368d64a9bc6bf0cc07685df4
PAULA REGINA CARDOSO BRUNO <79319807+paulinhabruno@users.noreply.github.com>
2021-03-14 20:44:05 -0300

    Atualização: test list

---
+++ src/main/kotlin/one/digitalinnovation/collection/TestList.kt
@@ -0,0 +1,38 @@
+package one.digitalinnovation.collection
+
+fun main() {
+
+    val joao = Funcionario(nome = "João", salario = 2000.0, "CLT")
+    val pedro = Funcionario(nome = "Pedro", salario = 1500.0, "PJ")
+    val maria = Funcionario(nome = "Maria", salario = 4000.0, "CLT")
+
+    val funcionarios = listOf(joao, pedro, maria)
+
+    funcionarios.forEach { println(it) }
+    println("--imprime a lista certinha de funcionário e seu salário. Se quiser imprimir somente 1, faça ---")
+
+    println(funcionarios.find { it.nome =="Maria" })
+
+    println("-- encadeamento de operações sobre uma colection. Neste caso vai imprimir os funcionários tendo os salarios em ordem e não pela letra  ---")
+
+    funcionarios.sortedBy { it.salario }.forEach { println(it) }
+
+    println("-- Cria uma lista pelo tipo de contratacao ---")
+
+    funcionarios.groupBy { it.tipoContratacao }.forEach { println(it) }
+
+}
+
+data class Funcionario(
+    val nome: String,
+    val salario: Double,
+    val tipoContratacao: String
+) {
+    override fun toString(): String =
+        """
+            Nome:    $nome
+            Salário: $salario
+        """.trimIndent()
+
+}
+
